2104e7d0b125e38cf50b283d52832d4837112492
Matthew Martin <mmartin724@yahoo.com>
2016-01-30 01:52:59 -0500

    Player movement and camera scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+
+	public float xMargin = 1f;
+	public float yMargin = 1f;
+	public float xSmooth = 8f;
+	public float ySmooth = 8f;
+
+	public Vector2 minXAndY;
+	public Vector2 maxXAndY;
+
+	private Transform target;
+
+	// Use this for initialization
+	void Start () 
+	{
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		TrackPlayer();
+	}
+
+	bool CheckXMargin()
+	{
+		return Mathf.Abs(transform.position.x - target.position.x) > xMargin;
+	}
+
+	bool CheckYMargin()
+	{
+		return Mathf.Abs(transform.position.y - target.position.y) > yMargin;
+	}
+
+	void TrackPlayer()
+	{
+		float targetX = target.position.x;
+		float targetY = target.position.y;
+
+		if(CheckXMargin())
+		{
+			targetX = Mathf.Lerp(transform.position.x, target.position.x, xSmooth * Time.deltaTime);
+		}
+
+		if(CheckYMargin())
+		{
+			targetY = Mathf.Lerp(transform.position.y, target.position.y, ySmooth * Time.deltaTime);
+		}
+
+		targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+	public bool facingRight = true;
+	public bool canJump = false;
+	private bool grounded = false;
+
+	public float maxSpeed = 5f;
+	public float moveForce = 5f;
+	public float jumpForce = 5f;
+
+	private Rigidbody2D rigid;
+	private Animator anim;
+
+	public Transform groundCheck;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rigid = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		anim.SetBool("Ground", grounded);
+
+		if(Input.GetButtonDown("Jump") && grounded)
+		{
+			canJump = true;
+			anim.SetBool("Ground", false);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+
+		anim.SetFloat("Speed", Mathf.Abs(horizontal));
+		anim.SetFloat("vSpeed", rigid.velocity.y);
+
+		if(horizontal * rigid.velocity.x < maxSpeed)
+		{
+			rigid.AddForce(Vector2.right * horizontal * moveForce);
+		}
+
+		if(Mathf.Abs(rigid.velocity.x) > maxSpeed)
+		{
+			rigid.velocity = new Vector2(Mathf.Sign(rigid.velocity.x) * maxSpeed, rigid.velocity.y);
+		}
+
+		if(horizontal > 0 && !facingRight)
+		{
+			Flip();
+		}
+		else if(horizontal < 0 && facingRight)
+		{
+			Flip();
+		}
+
+		if(canJump)
+		{
+			rigid.AddForce(new Vector2(0, jumpForce));
+
+			canJump = false;
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
