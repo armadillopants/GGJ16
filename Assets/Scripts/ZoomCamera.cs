ed2425a64d8bd4ef820238391d6b4ae22a6508ea
Matthew Martin <mmartin724@yahoo.com>
2016-01-30 12:42:32 -0500

    trying to fix zooming

--- Assets/Scripts/ZoomCamera.cs
+++ Assets/Scripts/ZoomCamera.cs
@@ -4,38 +4,69 @@ using System.Collections;
 [RequireComponent(typeof(BoxCollider2D))]
 public class ZoomCamera : MonoBehaviour 
 {
-	private float curZoom;
-	private float minZoom = 5f;
+	private float originalPos;
 	private float maxZoom = 12f;
 
-	private bool canZoom = true;
+	private bool canZoom = false;
 
 	public float zoomTime = 5f;
 
+	private float lastZoom;
+
+	Camera cam;
+
 	void Start () 
 	{
-		curZoom = minZoom;
+		GameObject camObject = GameObject.FindGameObjectWithTag("MainCamera");
+		cam = camObject.GetComponent<Camera>();
+
+		originalPos = cam.orthographicSize;
+		lastZoom = originalPos;
 	}
+
+	void Update()
+	{
+		if(canZoom)
+		{
+			if(lastZoom == minZoom)
+			{
+				curZoom = Mathf.Lerp(curZoom, maxZoom, zoomTime * Time.deltaTime);
+				cam.orthographicSize = curZoom;
+
+				if(curZoom == maxZoom)
+				{
+					cam.orthographicSize = maxZoom;
+					lastZoom = maxZoom;
+				}
+			}
+			else if(lastZoom == maxZoom)
+			{
+				curZoom = Mathf.Lerp(curZoom, minZoom, zoomTime * Time.deltaTime);
+				cam.orthographicSize = curZoom;
+
+				if(minZoom - curZoom < Mathf.Epsilon)
+				{
+					cam.orthographicSize = minZoom;
+					lastZoom = minZoom;
+				}
+			}
+		}
+	}
+
 
 	void OnTriggerEnter2D()
 	{
-		StartCoroutine(CameraZoom());
+		if(lastZoom == originalPos)
+		{
+			StartCoroutine(ZoomOut());
+		}
+		else if(lastZoom == maxZoom)
+		{
+		}
 	}
 
-	IEnumerator CameraZoom()
+	IEnumerator ZoomOut()
 	{
-		GameObject camObject = GameObject.FindGameObjectWithTag("MainCamera");
-		Camera cam = camObject.GetComponent<Camera>();
-
-		if(curZoom < maxZoom)
-		{
-			curZoom = Mathf.Lerp(curZoom, maxZoom, zoomTime * Time.deltaTime);
-			cam.orthographicSize = curZoom;
-			yield return null;
-		}
-		else
-		{
-			
-		}
+		
 	}
 }
