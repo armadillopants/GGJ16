a5044924b158e3006722f45f3f24594b433787c3
Matthew Martin <mmartin724@yahoo.com>
2016-01-31 02:00:58 -0500

    changes and updates

--- Assets/Scripts/ChoiceManager.cs
+++ Assets/Scripts/ChoiceManager.cs
@@ -28,13 +28,26 @@ public class ChoiceManager : MonoBehaviour
 		GameObject finalChoice = GameObject.Find("FinalChoice");
 		TextTrigger trigger = finalChoice.GetComponent<TextTrigger>();
 
-		if(choice > 0)
+		switch(choice)
 		{
-			trigger.storyText = "He came to accept his new reality, and the choices he made to get there truly showed where he was headed.";
-		}
-		else
-		{
-			trigger.storyText = "He never fully accepted his reality, and the choices he made to get there ultimately displayed his doomed destiny.";
+			case 3:
+				trigger.storyText = "He came to accept his new reality, and the choices he made to get there truly showed where he was headed.";
+				break;
+			case 2:
+				trigger.storyText = "He stumbled along the way, but in the end he came to appreciate this thing he once called life.";
+				break;
+			case 1:
+				trigger.storyText = "He had reached the end of his journey, but he had a lot more work to do.";
+			break;
+			case -1:
+				trigger.storyText = "He had one shot, and he blew it. One decision had changed it all.";
+				break;
+			case -2:
+				trigger.storyText = "He nearly made it, but his faults were too great to save him. He was doomed from the start.";
+				break;
+			case -3:
+				trigger.storyText = "He never fully accepted his reality, and the choices he made to get there ultimately displayed his doomed destiny.";
+				break;
 		}
 	}
 
